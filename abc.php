50dc05b80b0395e2affa1b5e89f2511edf029032
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2019-10-13 10:20:04 +0600

    Add files via upload

---
+++ abc.php
@@ -0,0 +1,53 @@
+<?php
+
+//$a = 0;
+//$b = "webtech";
+
+//$array = array(5,6);
+//$array =[5,'test'];
+//$array =["id"=>5,"name"=>'test'];
+
+//echo $array['name'];
+
+ $username= $_POST['username'];
+echo $username;
+ $password= $_POST['password'];
+ echo "<br>";
+echo $password ; 
+
+
+
+//$array2 = [	['XYZ','10-00000-1'],['ABC','10-00000-2'],['DEF','10-00000-3'] ];
+//echo $array2[0][0];
+
+
+
+//echo "dfvhsdbfhjsd dfhsdfh sd"." webtech"." sjfhgsdfh";
+//$c = print('Hello PHP');
+
+//echo "<br>".$c;
+
+/*	if(print('test') == 1){
+		echo "printed!";
+	}else{
+		echo "not printed!";
+	}*/
+
+/*	for($i=0; $i<10; $i++){
+		echo "test<br>";
+	}*/
+
+/*	function sum($a=0, $b=0){
+
+		echo $a+$b;
+		//return $a+$b;
+	}
+
+	sum(5);*/
+
+?>
+
+
+
+
+
